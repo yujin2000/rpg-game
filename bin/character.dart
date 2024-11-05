45da7b532c55a8588e321ebd446e2f1688a07a19
imeujin <imeujin.dev@gmail.com>
2024-11-05 15:31:33 +0900

    [feature] 캐릭터, 몬스터 공격/방어 기능 추가
    - 캐릭터, 몬스터의 공격/방어 기능 추가
    - Monster 클래스에 최대 공격력 속성 추가
    - 몬스터의 기본 공격력 지정 메서드 추가

--- bin/character.dart
+++ bin/character.dart
@@ -1,3 +1,5 @@
+import 'monster.dart';
+
 class Character {
   String name;
   int stamina;
@@ -5,6 +7,20 @@ class Character {
   int defense;
 
   Character(this.name, this.stamina, this.attack, this.defense);
+
+  void attackMonster(Monster monster) {
+    monster.stamina -= attack;
+  }
+
+  // 방어하기
+  // 몬스터가 입히는 데미지 만큼 캐릭터의 체력 상성
+  void defend(Monster monster) {
+    monster.setAttack(defense);
+    int realAttackValue = defense - attack;
+    monster.stamina += realAttackValue;
+    print('$name이(가) 방어 태세를 취하여 $realAttackValue 만큼 체력을 얻었습니다.');
+    showStatus();
+  }
 
   void showStatus() {
     print('$name - 체력: $stamina, 공격력: $attack, 방어력: $defense');
--- bin/monster.dart
+++ bin/monster.dart
@@ -1,12 +1,35 @@
+import 'dart:math';
+
+import 'character.dart';
+
 class Monster {
   String name;
   int stamina;
+  int maxAttack;
   int attack;
   int defense;
 
-  Monster(this.name, this.stamina, this.attack, {this.defense = 0});
+  Monster(this.name, this.stamina, this.maxAttack,
+      {this.attack = 0, this.defense = 0});
+
+  // 캐릭터 공격
+  void attackCharacter(Character character) {
+    int charDefense = character.defense;
+    setAttack(charDefense);
+    int realAttackValue = charDefense - attack;
+    character.stamina -= realAttackValue;
+
+    print('$name이(가) ${character.name}에게 $realAttackValue 만큼 데미지를 입혔습니다.');
+    character.showStatus();
+    showStatus();
+  }
+
+  // 캐릭터 최대 공격력 범위 내에서 랜덤으로 공격력을 추출하여 캐릭터 방어력과 max 비교하여 공격력 지정
+  void setAttack(int charDefense) {
+    attack = max(charDefense, Random().nextInt(maxAttack));
+  }
 
   void showStatus() {
-    print('$name - 체력: $stamina, 공격력: $attack, 방어력: $defense');
+    print('$name - 체력: $stamina, 최대공격력: $maxAttack, 방어력: $defense');
   }
 }
